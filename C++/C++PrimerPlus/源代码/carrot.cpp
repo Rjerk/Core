17c4074de408e8171e6552a1360bfcbd422891d8
Rjerk <460139918@qq.com>
2015-08-02 16:13:33 +0800

    Add C++ Primer Plus

---
+++ C++/C++PrimerPlus/源代码/carrot.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int carrots;
+
+	carrots = 25;
+	cout << "I have " << carrots << " carrots." << endl 
+	     << carrots - 1 << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+	return 0;
+}
+
