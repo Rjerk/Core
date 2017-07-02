ef18a7eebc6bbb434c8a1cfa9380cdd8b78dbe87
Rjerk <460139918@qq.com>
2017-07-02 10:19:15 +0800

    add zlib prec

--- linux-config/install.sh
+++ linux-config/install.sh
@@ -40,5 +40,10 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt update && sudo apt install -y sublime-text
 
+echo "dependency for nginx"
+sudo apt-get -y install libpcre3 libpcre3-dev
+sudo apt-get install zlib1g-dev
+
+
 
 
