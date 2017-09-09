154221eaaa2bc053e16016070e47d362e3b8c7cb
Rjerk <460139918@qq.com>
2017-09-09 11:14:23 -0400

    centos

---
+++ linux-config/centos_config.sh
@@ -0,0 +1,6 @@
+yum -y install git
+
+yum -y install gcc
+
+yum -y install gcc-c++
+
---
+++ linux-config/ubuntu_config.sh
@@ -0,0 +1,52 @@
+echo "---install p7zip---";
+sudo apt-get -y install p7zip-full p7zip-rar
+
+echo "---install boost---";
+sudo apt-get -y install libboost-all-dev
+
+echo "---install vim---";
+sudo apt-get -y install vim
+
+echo "---install git---";
+sudo apt-get -y install git
+
+echo "---install k-vim dependency---";
+sudo apt-get install exuberant-ctags
+sudo apt-get install build-essential cmake python-dev
+sudo apt-get install silversearcher-ag
+
+sudo apt-get install -y python-pip
+
+sudo pip install --upgrade pip
+
+sudo pip install pyflakes
+sudo pip install pylint
+sudo pip install pep8
+
+sudo apt-get install nodejs npm
+sudo npm install -g jslint
+sudo npm install jshint -g
+
+git clone https://github.com/wklken/k-vim.git
+cd k-vim
+sh -x install.sh
+
+# remove k-vim: cd ~ && rm -rf .vim .vimrc .vimrc.bundles && cd -
+
+sudo apt-get install -y curl
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y build-essential
+
+echo "---install hexo---";
+sudo npm isntall -g hexo-cli
+
+echo "dependency for nginx"
+sudo apt-get -y install libpcre3 libpcre3-dev
+sudo apt-get install zlib1g-dev
+
+echo "man"
+sudo apt-get -y install manpages-dev manpages-posix manpages-posix-dev libstdc++-6-doc glibc-doc 
+
+echo "valgind"
+sudo apt-get -y install valgrind
